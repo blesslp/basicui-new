a1cebfce5a980e9de98a284914db5332242de61c
blesslp <136002745@qq.com>
2019-08-06 17:11:10 +0800

    HttpLog fix

--- app/src/main/java/top/blesslp/http/HttpLogger.java
+++ app/src/main/java/top/blesslp/http/HttpLogger.java
@@ -1,10 +1,12 @@
 package top.blesslp.http;
 
-import android.util.Log;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.LogcatLogStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -14,6 +16,16 @@ public class HttpLogger implements HttpLoggingInterceptor.Logger {
     private StringBuilder mMessage = new StringBuilder();
     public HttpLogger(String tag) {
         this.tag = tag;
+        Logger.clearLogAdapters();
+        PrettyFormatStrategy strategy = PrettyFormatStrategy.newBuilder()
+                .tag(tag)
+                .methodOffset(0)
+                .methodCount(0)
+                .showThreadInfo(false)
+                .logStrategy(new LogcatLogStrategy())
+                .build();
+        AndroidLogAdapter logAdapter = new AndroidLogAdapter(strategy);
+        Logger.addLogAdapter(logAdapter);
     }
 
     @Override
@@ -32,14 +44,14 @@ public class HttpLogger implements HttpLoggingInterceptor.Logger {
         }
 
         if (message.startsWith("--> END")) {
-            Log.w(tag,mMessage.toString());
+            Logger.w(mMessage.toString());
             mMessage.setLength(0);
             return;
         }
 
         // 响应结束，打印整条日志
         if (message.startsWith("<-- END HTTP")) {
-            Log.w(tag,mMessage.toString());
+            Logger.w(mMessage.toString());
             return;
         }
 
